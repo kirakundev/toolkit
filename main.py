428283bb45d8f17c0e938b3d179877e2e880cb0b
Ers ID <83586926+WarceuProject@users.noreply.github.com>
2022-05-12 00:56:02 +0700

    update

--- main.py
+++ main.py
@@ -2,12 +2,20 @@
 import os,sys
 def updatae():
     os.system('git pull')
-def banner():
+def main():
     os.system('clear')
     print """
          
             [#] Warceu Project Toolkit [#]
         ______________________________________
 
+        Menu:
+             (1).Inatall require packagess
+             (2).Start toolkit
+             (3).Update
+             (5).About
+             (0).Exit program
+
         """
-banner()
+main()
+pil = input("pilih:")
