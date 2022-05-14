ceff42ef037624772fe70097eb83622faf2f3eb9
lightid180@gmail.com <lightid180@gmail.com>
2022-05-14 13:06:24 +0700

    upadate

--- asst/about.sh
+++ asst/about.sh
@@ -1,6 +1,22 @@
 clear
 echo """
         [#] About Informations Toolkit [#]
+        
+        
+    [*] About information
+    
+    Author: Ers ID
+    Project: Toolkit v.0.01
+    Organitation: Warceu Project Center
+    Language packages: python2.7,bash shell,php,ruby
+    Feature: Toolkit all installer tool program
+    
+    [*] Contact US 
+    
+    Whatsapp: +6285759669252
+    Facebook: facebook.com/warceuproject
+    Github: github.com/WarceuProject
+    Forum : te.me/warceuprojectcenter
      """
     read -p "[?]Prees enter to back home..."
     
