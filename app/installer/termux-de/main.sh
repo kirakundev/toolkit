97a2b5765a2d3a4a6a2fc1cf6405c449c72a0f83
Ers ID <lightid180@gmail.com>
2022-05-15 18:30:55 +0000

    update

---
+++ app/installer/termux-de/main.sh
@@ -0,0 +1,14 @@
+clear
+echo $"""
+      [#] welcome the termux desktop installer [#]
+    ________________________________________________
+
+    Menu:
+         1.termux DE with Ers ID (try this which is under development)
+         2.termux DE with Adi1090x
+         3.termux DE with Yizus7u7 
+         0.exit 
+         """
+         read -p "chosse: " cos;
+
+         
