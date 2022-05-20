3450799f97cb21471b33f7c1a7ad80e4f6332e27
lightid180@gmail.com <lightid180@gmail.com>
2022-05-20 13:59:57 +0700

    try

--- app/installer/termux-de/td2.sh
+++ app/installer/termux-de/td2.sh
@@ -1,5 +1,5 @@
 read -p "Enter to continue"
 cd $HOME
-wget https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/boostrap.sh --no-check-certificate
+wget https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/boostrap.sh --no-check-certificate 
 chmod 777 boostrap.sh
 bash boostrap.sh
