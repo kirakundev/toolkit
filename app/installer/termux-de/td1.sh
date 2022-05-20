20b11bb5917e9c2b653bec4ef1b8771ddea78063
lightid180@gmail.com <lightid180@gmail.com>
2022-05-20 13:32:44 +0700

    up td

--- app/installer/termux-de/td1.sh
+++ app/installer/termux-de/td1.sh
@@ -1,2 +1,4 @@
 cd $HOME
-wget 
+wget https://raw.githubusercontent.com/adi1090x/termux-desktop/master/setup.sh --no-check-certificate 
+chmod 777 setup.sh
+bash setup.sh --install
---
+++ app/installer/termux-de/td2.sh
@@ -0,0 +1,4 @@
+cd $HOME
+wget https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/boostrap.sh --no-check-certificate
+chmod 777 boostrap.sh
+bash boostrap.sh
